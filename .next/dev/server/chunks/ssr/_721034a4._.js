f8be3a6fa09ccbc8ac2cd08c1b680afa416812ec
Nicolas Velasco Perez <velasconicolas84@gmail.com>
2025-12-10 17:33:32 -0500

    Updated img, fixed wpp issue, updated copy

--- .next/dev/server/chunks/ssr/_721034a4._.js
+++ .next/dev/server/chunks/ssr/_721034a4._.js
@@ -114,7 +114,7 @@ function PhoneMockup() {
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                         className: "relative w-full h-full bg-[#0a0a0a] rounded-[32px] overflow-hidden",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                            src: "recobra-app-screenshot.png",
+                            src: "/recobra-app-screenshot-active.jpg",
                             alt: "Recobra App - Listo para enfocarte",
                             className: "w-full h-full object-contain pt-8"
                         }, void 0, false, {
--- .next/dev/static/chunks/_42a98bb0._.js
+++ .next/dev/static/chunks/_42a98bb0._.js
@@ -123,7 +123,7 @@ function PhoneMockup() {
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                         className: "relative w-full h-full bg-[#0a0a0a] rounded-[32px] overflow-hidden",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                            src: "recobra-app-screenshot.png",
+                            src: "/recobra-app-screenshot-active.jpg",
                             alt: "Recobra App - Listo para enfocarte",
                             className: "w-full h-full object-contain pt-8"
                         }, void 0, false, {
