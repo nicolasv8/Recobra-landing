ae9e9ce0aefaec1a3137ee4ac266466ac7f4a3c1
Nicolas Velasco Perez <velasconicolas84@gmail.com>
2026-03-04 16:15:50 -0500

    feat: update Content Security Policy to include Google Tag Manager and Google Analytics domains.

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value:
-      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://va.vercel-scripts.com https://vitals.vercel-insights.com https://*.wompi.co https://production.wompi.co; frame-ancestors 'none'; base-uri 'self'; form-action 'self';",
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://va.vercel-scripts.com https://vitals.vercel-insights.com https://*.wompi.co https://production.wompi.co https://*.googletagmanager.com https://*.google-analytics.com; frame-ancestors 'none'; base-uri 'self'; form-action 'self';",
   },
 ]
 
